348387de6c85e9313204c73828073dcfe46d654f
joshiCoding <joshi.mohit1221@gmail.com>
2020-04-22 02:24:04 +0530

    added database connectivity for first section

--- js/main.js
+++ js/main.js
@@ -43,3 +43,57 @@ menuBtnClose.addEventListener('click', e =>{
     }
    
 })
+
+
+// using the fetch api here
+function getData(){
+    fetch('https://api.covid19india.org/data.json')
+    .then(res => res.json())
+    .then( data =>{
+        console.log(data);
+        fillCurrentSituation(data);
+
+    })
+}
+
+function fillCurrentSituation(data){
+    console.log(data.cases_time_series[data.cases_time_series.length -1]);
+
+    const today = data.cases_time_series[data.cases_time_series.length -1];
+
+    const activeNo = document.querySelector('.stat_active .stat_number');
+    const activeChangeNo = document.querySelector('.stat_active .stat_change');
+
+    const confirmedNo = document.querySelector('.stat_confirmed .stat_number');
+    const confirmedChangeNo = document.querySelector('.stat_confirmed .stat_change');
+
+    const recoveredNo = document.querySelector('.stat_recovered .stat_number');
+    const recoveredChangeNo = document.querySelector('.stat_recovered .stat_change');
+
+    const deathNo = document.querySelector('.stat_death .stat_number');
+    const deathChangeNo = document.querySelector('.stat_death .stat_change');
+
+    const statsFor = document.querySelector('.updated b');
+
+    
+    let activeSum = parseInt(today.totalconfirmed) - parseInt(today.totalrecovered) - parseInt(today.totaldeceased);
+    let activeDiff = parseInt(today.dailyconfirmed) - parseInt(today.dailyrecovered) - parseInt(today.dailydeceased);
+   
+
+    confirmedNo.innerText = today.totalconfirmed;
+    confirmedChangeNo.innerText = "+" + today.dailyconfirmed;
+
+    activeNo.innerText = activeSum;
+    activeChangeNo.innerText = "+" + activeDiff;
+
+    recoveredNo.innerText = today.totalrecovered;
+    recoveredChangeNo.innerText = "+" + today.dailyrecovered;
+
+    deathNo.innerText = today.totaldeceased;
+    deathChangeNo.innerText = "+" + today.dailydeceased;
+
+    statsFor.innerText = today.date;
+
+    
+}
+getData();
